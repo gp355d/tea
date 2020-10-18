6069564342891bcca0f307b312a603c2a077b967
leo <chichali374@gmail.com>
2020-10-18 08:56:43 +0800

    新增vue router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
+  <router-link class="navbar-brand" to="/">tea</router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+    <div class="navbar-nav font-weight-bold">
+      <router-link class="nav-item nav-link" to="/products">前台產品列表<span class="sr-only">(current)</span></router-link>
+      <router-link class="nav-item nav-link" to="/about">關於我們</router-link>
+    </div>
+  </div>
+</nav>
+</template>
---
+++ src/views/frontend/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Navbar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '../../assets/all.scss';
+</style>
