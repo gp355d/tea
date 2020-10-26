d5c69a55f584f1cf461415357a734a1eb8018f2b
leo <chichali374@gmail.com>
2020-10-26 15:01:57 +0800

    增加使用bootstrap所需套件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import jquery from 'jquery'
+import 'bootstrap'
 Vue.config.productionTip = false
-
+window.$ = jquery
 new Vue({
   router,
   render: h => h(App)
